c64963c2b18158767f915f2921cb3bde5d04751b
shanyao0 <15910489700@163.com>
2016-07-11 11:24:32 +0800

    将下载的代码转移到服务中去后台运行

--- app/src/main/java/shanyao/downloadmanager/MainActivity.java
+++ app/src/main/java/shanyao/downloadmanager/MainActivity.java
@@ -1,10 +1,7 @@
 package shanyao.downloadmanager;
 
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-
-import java.io.File;
 
 import shanyao.downloadmanager.manager.UpdateManager;
 
@@ -14,9 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String fileName = "app-debug.apk";
-        String fileStoreDir = Environment.getExternalStorageDirectory().getAbsolutePath() + File
-                .separator + "M_DEFAULT_DIR";
-        new UpdateManager(this,fileStoreDir,fileName).checkUpdate(false);
+        new UpdateManager(this).checkUpdate(false);
     }
 }
